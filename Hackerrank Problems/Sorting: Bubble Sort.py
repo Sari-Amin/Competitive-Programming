0c1936d6319c78cd4357b65bc6b46aadf5103f93
Sari-Amin <58248593+Sari-Amin@users.noreply.github.com>
2023-07-31 10:49:14 +0000

    solved sorting: bubble sort on hackerrank

---
+++ Hackerrank Problems/Sorting: Bubble Sort.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def countSwaps(a):
+    swap = 0
+    for i in range(len(a)):
+        for j in range(len(a)-1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1] , a[j]
+                swap += 1
+    print(f"Array is sorted in {swap} swaps.\nFirst Element: {a[0]}\nLast Element: {a[-1]}")
+                
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
