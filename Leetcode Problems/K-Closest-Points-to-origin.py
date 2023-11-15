d6ca51c813897febe73b93aeddc019ebca53195c
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2023-11-15 22:47:33 +0300

    Update K-Closest-Points-to-origin.py

--- Leetcode Problems/K-Closest-Points-to-origin.py
+++ Leetcode Problems/K-Closest-Points-to-origin.py
@@ -1,10 +1,3 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        ans = []
-        distance = []
-        for i in range(len(points)):
-            distance.append([(points[i][0] * points[i][0] + points[i][1] * points[i][1]), i])
-        distance.sort()
-        for i in range(k):
-            ans.append(points[distance[i][1]])
-        return ans
+        return sorted(points, key = lambda p: p[0]*p[0] + p[1]*p[1])[:k]
