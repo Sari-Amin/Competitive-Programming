fb0a0612718e70acde2f2c7058390d3d24b4a569
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2023-11-19 22:47:55 +0300

    Update Longest-subsequence-with-limited-sum.py

--- Leetcode Problems/Longest-subsequence-with-limited-sum.py
+++ Leetcode Problems/Longest-subsequence-with-limited-sum.py
@@ -6,9 +6,6 @@ class Solution:
         for j in range(len(queries)):
             lsum = 0
             temp = 0
-            if queries[j] >= total:
-                ans.append(len(nums))
-                continue
             if queries[j] < nums[0]:
                 ans.append(0)
                 continue
