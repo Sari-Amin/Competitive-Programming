449ead57ef3381f60641a8a12739c0de78cde85f
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2023-11-23 20:34:30 +0300

    Update Two-Sum.py

--- Leetcode Problems/Two-Sum.py
+++ Leetcode Problems/Two-Sum.py
@@ -1,8 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         ans = [0,0]
+        dic = {}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+            if target - nums[i] in dic:
+                ans = [dic[target-nums[i]],i]
+                break
+            dic[nums[i]] = i
         return ans
