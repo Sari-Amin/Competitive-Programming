ac03287864d47ecc8b0532c71bd30bcc74b51731
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2023-11-29 20:36:45 +0300

    Create Add-strings.py

---
+++ Leetcode Problems/Add-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        carry = 0
+        ans = ""
+        i,j = len(num1)-1,len(num2)-1
+        while i >= 0 or j >= 0 or carry != 0:
+            if i >= 0 :
+                carry += int(num1[i])
+                i -= 1
+            if j >= 0 :
+                carry += int(num2[j])
+                j -= 1
+            ans += str(carry % 10)
+            carry //= 10
+        return ans[::-1]
