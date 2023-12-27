626bdd47be108049915dd1edc60ece884d6075bc
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2023-12-27 22:16:03 +0300

    Create The-minion-game.py

---
+++ Hackerrank Problems/The-minion-game.py
@@ -0,0 +1,19 @@
+def minion_game(string):
+    # your code goes here
+    kevin = 0
+    stuart = 0
+    for i in range(len(string)):
+        if string[i] in "AEIOU":
+            kevin += len(string) - i
+        else:
+            stuart += len(string) - i
+    if kevin > stuart:
+        print("Kevin", kevin)
+    elif stuart > kevin:
+        print("Stuart", stuart)
+    else:
+        print("Draw")
+            
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
