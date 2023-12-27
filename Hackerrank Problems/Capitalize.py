0e7f7e866f3a7715c2a811304bd1fc0913cce6bf
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2023-12-27 13:52:47 +0300

    Create Capitalize.py

---
+++ Hackerrank Problems/Capitalize.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    ans = s[0].upper()
+    for i in range(1,len(s)):
+        if s[i-1] == " " and s[i].isalpha():
+            ans += s[i].upper()
+        else:
+            ans += s[i]
+    return ans
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
