81237c065693c9c0199beca6c01570d58f400e2f
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-01-04 22:49:43 +0300

    Update Minimum-number-of-operations-to-make-array-empty.py

--- Leetcode Problems/Minimum-number-of-operations-to-make-array-empty.py
+++ Leetcode Problems/Minimum-number-of-operations-to-make-array-empty.py
@@ -1,19 +1,9 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        dic = {}
-        for i in nums:
-            dic[i] = dic.get(i,0) + 1
-        ans = 0 
-        for i in dic:
-            if dic[i] % 3 == 0:
-                ans += dic[i] // 3
-            elif dic[i] != 1 and (dic[i] % 3) % 2 == 1:
-                ans += dic[i] // 3
-                ans -= 1
-                ans += (dic[i] % 3 + 3) // 2
-            elif (dic[i] % 3) % 2 == 0:
-                ans += dic[i] // 3
-                ans += (dic[i] % 3) // 2
-            else:
+        counter = Counter(nums)
+        ans = 0
+        for val in counter:
+            if counter[val] == 1:
                 return -1
+            ans += math.ceil(counter[val]/3)
         return ans
