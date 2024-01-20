2c47af49c5b2a98704008c5b43523921420efab9
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-01-20 11:12:45 +0300

    Create Decode-string.py

---
+++ Leetcode Problems/Decode-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        st = []
+        ans = ""
+        for i in s:
+            if i != "]":
+                st.append(i)
+            else:
+                temp = []
+                while len(st) > 0 and st[-1].isalpha():
+                    temp.append(st.pop())
+                st.pop()
+                num = ""
+                while len(st) > 0 and st[-1] in "0123456789":
+                    num += st.pop()
+                st.append("".join(temp[::-1]) * int(num[::-1]))
+                
+        return "".join(st)
