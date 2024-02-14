f59356faf2228a9e529a9e25df5f2a761a6d4dd7
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-02-14 14:23:27 +0300

    Delete A2SV-Progress-Sheet/leetcode directory

--- A2SV-Progress-Sheet/leetcode/k-items-with-the-maximum-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
-        if numOnes >= k:
-            return k
-        else:
-            if numOnes + numZeros >= k:
-                return numOnes
-            else:
-               return 2 * numOnes - k  + numZeros
