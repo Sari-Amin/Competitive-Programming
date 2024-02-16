a90e3f566c174bf08b6726ca2d1090599c38812b
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-02-16 23:11:55 +0300

    Add solution for All Divisions With the Highest Score of a Binary Array

--- A2SV-Progress-Sheet/leetcode/all-divisions-with-the-highest-score-of-a-binary-array.py
+++ A2SV-Progress-Sheet/leetcode/all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -1,24 +1,18 @@
 class Solution:
     def maxScoreIndices(self, nums: List[int]) -> List[int]:
         ones = nums.count(1)
-        zeros = len(nums) - ones
         lsum = 0
-        res = []
-
+        ans = defaultdict(list)
+        best = -1
         for i in range(len(nums)):
-            res.append((ones + lsum, i))
+            ans[ones + lsum].append(i)
+            best = max(best, ones + lsum)
             if nums[i] == 0:
                 lsum += 1
             else:
                 ones -= 1
-        res.append((ones + lsum, i + 1))
-        res.sort(reverse = True)
-        
-        ans = [res[0][1]]
-        for i in range(1,len(res)):
-            if res[0][0] == res[i][0]:
-                ans.append(res[i][1])
-            else:
-                break
+            
+        ans[ones + lsum].append(i + 1)
+        best = max(best, ones + lsum)
 
-        return ans
+        return ans[best]
