d0a95a2d8c4c785190ceffb47df87adc844ba246
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-02-16 09:29:39 +0300

    Add solution for Least Number of Unique Integers after K Removals

--- A2SV-Progress-Sheet/leetcode/least-number-of-unique-integers-after-k-removals.py
+++ A2SV-Progress-Sheet/leetcode/least-number-of-unique-integers-after-k-removals.py
@@ -2,12 +2,11 @@ class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
         freq = list(Counter(arr).values())
         freq.sort()
-        ans = 0
         lsum = 0
         for i in range(len(freq)):
             if lsum + freq[i] <= k:
                 lsum += freq[i]
             else:
-                ans += 1
+                return len(freq) - i
 
-        return ans
+        return 0
