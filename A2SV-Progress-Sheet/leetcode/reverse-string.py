8703bb7ef809fb14a4818fe9b9b5dba3bc094498
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-02-20 23:05:52 +0300

    Add solution for Reverse String

--- A2SV-Progress-Sheet/leetcode/reverse-string.py
+++ A2SV-Progress-Sheet/leetcode/reverse-string.py
@@ -3,4 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s[:] = s[::-1]
+        def rec(l, r):
+            if l >= r:
+                return 
+            s[l], s[r] = s[r], s[l]
+            return rec(l + 1, r - 1)
+
+        r = len(s) - 1
+        l = 0
+        rec(l, r)
+     
