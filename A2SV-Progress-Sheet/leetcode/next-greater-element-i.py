86af50a656bd69464cddf2b8a84496ca2f894834
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-02-22 07:46:24 +0300

    Add solution for Next Greater Element I

--- A2SV-Progress-Sheet/leetcode/next-greater-element-i.py
+++ A2SV-Progress-Sheet/leetcode/next-greater-element-i.py
@@ -1,18 +1,13 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        ans = []
-        for i in range(len(nums1)):
-            stack = []
-            for j in nums2:
-                if nums1[i] == j:
-                    stack.append(j)
-                else:
-                    if stack and stack[-1] < j:
-                        ans.append(j)
-                        stack.pop()
-                        break
-            if stack and stack[-1] == nums1[i]:
-                ans.append(-1)
-        return ans
+        stack = []
+        dic = defaultdict(lambda : -1)
+        for num in nums2:
+            while stack and num > stack[-1]:
+                dic[stack[-1]] = num
+                stack.pop()
+            stack.append(num)
+
+        return [dic[num] for num in nums1]
 
                     
