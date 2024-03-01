1a1353769e748a604f762737f44e9ac786b788f6
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-03-01 05:25:07 +0300

    Add solution for Maximum Odd Binary Number

---
+++ A2SV-Progress-Sheet/leetcode/maximum-odd-binary-number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        ones = s.count("1")
+        if ones == 0:
+            return s
+        return "1" * (ones - 1) + "0" * (len(s) - ones) + "1"
