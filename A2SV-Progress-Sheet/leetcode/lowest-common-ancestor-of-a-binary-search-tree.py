3e9876b946900c1d3e8c2234feef5f26e3ca5691
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-03-05 14:25:36 +0300

    Add solution for Lowest Common Ancestor of a Binary Search Tree

--- A2SV-Progress-Sheet/leetcode/lowest-common-ancestor-of-a-binary-search-tree.py
+++ A2SV-Progress-Sheet/leetcode/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -9,12 +9,19 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if not root:
             return None
+
+        #if one is the ancestor of the other
         if root.val == p.val or root.val == q.val:
             return root
+
+        #if one is in left subtree and the other in right subtree 
         if q.val > root.val and p.val < root.val or q.val < root.val and p.val > root.val:
             return root
+
+        #if both are in the left subtree
         if q.val < root.val and p.val < root.val:
             return self.lowestCommonAncestor(root.left,p,q)
-        
+            
+        #if both are in the right subtree
         return self.lowestCommonAncestor(root.right,p,q)
         
