11c9de77eea06f3485b3fcb80771b23a69a8a650
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-03-05 15:36:29 +0300

    Add solution for Minimum Length of String After Deleting Similar Ends

--- A2SV-Progress-Sheet/leetcode/minimum-length-of-string-after-deleting-similar-ends.py
+++ A2SV-Progress-Sheet/leetcode/minimum-length-of-string-after-deleting-similar-ends.py
@@ -2,23 +2,11 @@ class Solution:
     def minimumLength(self, s: str) -> int:
         left = 0
         end = len(s) - 1
-        while left < end:
-            if s[left] == s[end]:
-                temp = s[left]
-                while left < end:
-                    if temp == s[left]:
-                        left += 1
-                    else:
-                        break
-                while end > left:
-                    if temp == s[end]:
-                        end -= 1
-                    else:
-                        break
-            else:
-                break
-        ans = end - left + 1
-        if left == end and (end + 1 < len(s) and s[left] == s[end + 1] or left - 1 >= 0 and s[left - 1] == s[end]): 
-            ans -= 1 
-        
-        return ans
+        while left < end and s[left] == s[end]:
+            temp = s[left]
+            while left <= end and temp == s[left]:
+                left += 1
+            while end >= left and temp == s[end]:
+                end -= 1
+            
+        return end - left + 1
